c2b00ef64521464b27b60042761a9467eaca7c47
Dayroot <dallancaceresp@gmail.com>
2022-01-28 15:21:06 -0500

    Implement modal window

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
 // import gsap from "gsap";
 // import ScrollTrigger from "gsap/ScrollTrigger";
-// import Parallax from 'parallax-js'
+// import Parallax from "parallax-js"
 
-/* Gsap library usage statement */
+/*
+    Gsap library usage statement
+*/
 gsap.registerPlugin(ScrollTrigger);
 
-/* Classes of the elements to which an animation will be applied using gsap */
+/* 
+    Global parameter variables
+*/
+let sunOcultation = 1;
+
+/* 
+    Classes of the elements to which an animation will be applied using gsap 
+*/
 const parallaxSceneSelector = ".banner-parallax";
 const sunSelector = ".banner-parallax__item--sun";
 const sunImgSelector = ".banner-parallax__img--sun";
+const sunLight1Selector = "#sunLight1";
+const sunLight2Selector = "#sunLight2";
 const groundTransitionSelector = ".ground-transition";
 const sectDescripSelector = ".section-description";
-const sectDescripContentSelector = ".section-description__content"
+const sectDescripContentSelector = ".section-description__content";
+const backdropSelector = ".backdrop";
+const modalSelector = ".modal";
+const modalLightSelector = "#modalLight";
 
-/* Dom elements */
+/*
+    Dom elements 
+*/
 const parallaxScene = document.querySelector(parallaxSceneSelector);
-const buttonStart = document.querySelector('#buttonStart');
+const buttonStart = document.querySelector("#buttonStart");
+const modalContainer = document.querySelector("#modalContainer");
+const modalBackdrop = document.querySelector(backdropSelector);
+const sunLight1 = document.querySelector(sunLight1Selector);
+const sunLight2 = document.querySelector(sunLight2Selector);
 
-/* Parallax effect when moving the mouse, using the parallax-js library */
+/*
+    Parallax effect when moving the mouse, using the parallax-js library 
+*/
 const parallaxInstance  =  new  Parallax( parallaxScene );
 
-/* Motion animations when scrolling, using the gsap library*/
+/* 
+    Motion animations when scrolling, using the gsap library
+*/
 gsap.to(sunImgSelector, {
     yPercent: 100,
     ease: "none",
@@ -29,7 +53,7 @@ gsap.to(sunImgSelector, {
         start: "top top",
         end: "bottom top",
         scrub: true,
-    }
+    }  
 });
 gsap.to(sectDescripSelector, {
     yPercent: -15,
@@ -38,7 +62,141 @@ gsap.to(sectDescripSelector, {
         trigger: groundTransitionSelector,
         start: "top center",
         end: "bottom bottom",
-        scrub: true,
+        scrub: true
     }
 });
 
+/*
+    Gsap animation timelines
+*/
+const blinkSun = gsap.timeline({
+    repeat: -1,
+    yoyo: true
+});
+
+const blinkModalLight = gsap.timeline({
+    repeat: -1,
+    yoyo: true
+});
+
+/* 
+    Gsap animations based on timelines 
+*/
+blinkSun
+    .to(
+        sunLight1Selector, 
+        { 
+            duration: 0.8,
+            opacity: 0.6
+        },
+        0
+    )
+    .to(
+        sunLight2Selector, 
+        { 
+            duration: 0.8,
+            opacity: 0.4,
+        },
+        0
+    )
+    .to(
+        sunLight1Selector, 
+        { 
+            duration: 1,
+            opacity: 1,
+            ease:  "power1.out",
+        },
+        0.8
+    )
+    .to(
+        sunLight2Selector, 
+        { 
+            duration: 1,
+            opacity: 0.6,
+            ease:  "power1.out",
+        },
+        0.8
+    )
+
+blinkModalLight
+    .to(
+        modalLightSelector, 
+        { 
+            duration: 0.8,
+            opacity: 0.4,
+        }
+    )
+    .to(
+        modalLightSelector, 
+        { 
+            duration: 1,
+            opacity: 1,
+            ease:  "power1.out",
+        }
+    )
+
+
+
+/* 
+    Dom element events 
+*/
+buttonStart.onclick = function(){
+    const offsetY = window.pageYOffset || document.documentElement.scrollTop;
+    //disableScrollY(offsetY);
+    modalContainer.style.display = "";
+    modalContainer.style.top = `${offsetY}px`;
+    gsap.to(backdropSelector, {
+        opacity: 1,
+        duration: 0.15
+    });
+    gsap.to(modalSelector, {
+        scale: 1,
+        duration: 0.15
+    });
+}
+
+modalBackdrop.onclick = async function(){
+    await gsap.to(backdropSelector, {
+        opacity: 0,
+        duration: 0.1
+    });
+    await gsap.to(modalSelector, {
+        scale: 0,
+        duration: 0.05
+    });
+    modalContainer.style.display = "none";
+    //enableScrollY();
+}
+
+// function disableScrollY(offsetY) {
+//     window.scrollTop(offsetY);
+// }
+
+// function enableScrollY(){
+//     window.onscroll = function() {};
+// }
+
+/*
+    This funcion is responsible for establishing the opacity level of the light
+    as the element that represents the sun in the banner moves.
+*/
+window.onscroll = function(event) {
+    const OCCULTATION_COMPLETED = 300;
+    const OCCULTATION_START = 80;
+    const offSetY = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(offSetY);
+    if(offSetY > OCCULTATION_START){
+        blinkSun.pause();
+        if(offSetY < OCCULTATION_COMPLETED)
+            sunOcultation = 1 - offSetY/OCCULTATION_COMPLETED;
+        else
+            sunOcultation = 0;
+        sunLight2.style.opacity = sunOcultation;
+        }
+    else {
+        sunLight2.style.opacity = "";
+        blinkSun.play(); 
+    }
+    
+     
+}
